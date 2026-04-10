5c8eec813d6cb5fcbd4bf88ef94b2a0fbc52b19d
Heráclito <heraclitoqsaldanha@gmail.com>
2026-04-09 22:31:20 -0300

    ajuste no ChannelIdentifier

--- src/routes/socket_io/message.rs
+++ src/routes/socket_io/message.rs
@@ -2,34 +2,49 @@ use crate::*;
 
 use socketioxide::extract::*;
 use socketioxide::*;
+use socketioxide_core::adapter::*;
 
 const MESSAGE_PAGE_SIZE: i64 = 32;
 
-#[derive(Debug, Clone)]
-struct Channel(i64);
+#[derive(Debug, Clone, Copy)]
+struct ChannelIdentifier(i64);
+
+impl RoomParam for ChannelIdentifier {
+	type IntoIter = std::iter::Once<Room>;
+
+	fn into_room_iter(self) -> Self::IntoIter {
+		std::iter::once(Room::from(self.0.to_string()))
+	}
+}
 
 pub async fn join_channel(socket: SocketRef, Data(channel_id): Data<i64>, ack: AckSender) {
-	socket.leave_all();
-	socket.join(channel_id.to_string());
-	socket.extensions.insert(Channel(channel_id));
+	if let Some(channel) = socket.extensions.get::<ChannelIdentifier>() {
+		socket.leave(channel);
+	};
+	let channel = ChannelIdentifier(channel_id);
+
+	socket.join(channel);
+	socket.extensions.insert(channel);
+
 	let _ = ack.send(&true);
 }
 
 pub async fn on_send_message(io: SocketIo, socket: SocketRef, Data(content): Data<String>, State(app): State<app::AppState>) {
-	let Some(Channel(channel_id)) = socket.extensions.get::<Channel>() else {
+	let Some(channel) = socket.extensions.get::<ChannelIdentifier>() else {
 		return;
 	};
-	let Ok(message) = db::create_message(&app.db_pool, channel_id, &content).await else {
+	let Ok(message) = db::create_message(&app.db_pool, channel.0, &content).await else {
 		return;
 	};
-	let _ = io.to(channel_id.to_string()).emit("messageReceived", &message).await;
+
+	let _ = io.to(channel).emit("messageReceived", &message).await;
 }
 
 pub async fn on_load_messages(State(app): State<app::AppState>, socket: SocketRef, Data(before_id): Data<Option<i64>>, ack: AckSender) {
-	let Some(Channel(channel_id)) = socket.extensions.get::<Channel>() else {
+	let Some(channel) = socket.extensions.get::<ChannelIdentifier>() else {
 		return;
 	};
-	let messages = db::get_messages(&app.db_pool, channel_id, before_id, MESSAGE_PAGE_SIZE).await.unwrap_or_default();
+	let messages = db::get_messages(&app.db_pool, channel.0, before_id, MESSAGE_PAGE_SIZE).await.unwrap_or_default();
 
 	let _ = ack.send(&messages);
 }
